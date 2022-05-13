989c5055fc8b61f75f5ec69319c316c2ea00390a
Ryan Kadri <ryankadri.dev@gmail.com>
2022-05-13 10:05:25 +0200

    flag page style

---
+++ lib/flag.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drapeaux</title>
+    <link rel="stylesheet" href="/css/flag.css">
+</head>
+<body>
+    <div class="flag">
+        <h1>Les drapeaux !</h1>
+        <div class="btn-add">
+            <a href="/">Ajouter un drapeau</a>
+        </div>
+
+        <img src="/img/dz-flag.png" alt="Algérie">
+    </div>
+</body>
+</html>
